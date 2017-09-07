11b53c63b2d46a8ba80a1bfa06601fce13d8149b
finalboy <wkdeorb2002@naver.com>
2017-09-07 11:14:44 +0900

    BitProject

--- BitProject/WebContent/resources/js/productScript.js
+++ BitProject/WebContent/resources/js/productScript.js
@@ -22,3 +22,52 @@ function shoppingBasketCheck() {
     var top = (window.screen.height - windowH)/3;
     open(url,"찜 확인 페이지","top="+top+", left="+left+", height="+windowH+", width="+windowW);
 }
+
+function insertGrade() {
+	var gradeList =	document.getElementById("grade_list");
+	var date = new Date();
+	
+	var gradeNum = document.getElementById("g_grade_num").value;
+	var img_src = document.getElementById("g_img_src").value;
+	var content = document.getElementById("g_content").value;
+	var user_id = document.getElementById("g_user_id").value;
+	
+	var h5Tag = document.createElement("h5");
+	
+	h5Tag.innerHTML = "<div>"
+				+		"<h5>" + gradeNum + "   |   "
+				+		img_src + "   |   "
+				+		content + "   |   "
+				+		user_id + "   |   "
+				+		date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + "   |   "
+				+		"</h5>"
+				+	"</div>";
+	
+	gradeList.appendChild(h5Tag);
+	
+	return;
+}
+
+function insertInquiry() {
+	var inquiryList = document.getElementById("inquiry_list");
+	var date = new Date();
+	
+	var select = document.getElementById("i_type");
+	var type = select.options[select.selectedIndex].value;
+	var classify = document.getElementById("i_classify").value;
+	var user_id = document.getElementById("i_user_id").value;
+	
+	var h5Tag = document.createElement("h5");
+	
+	h5Tag.innerHTML = "<div>"
+					+		"<h5>" + type + "   |   "
+					+		classify + "   |   "
+					+		user_id + "   |   "
+					+		date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate() + "   |   "
+					+		"</h5>"
+					+	"</div>";
+	
+	inquiryList.appendChild(h5Tag);
+	
+	return;
+}
