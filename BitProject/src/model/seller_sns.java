d4393b6c90aaeb21be5d9f6a415f261cfa7b7937
Jang <wkdeorb2002@naver.com>
2017-09-27 10:54:26 +0900

    ...

--- BitProject/src/model/seller_sns.java
+++ BitProject/src/model/seller_sns.java
@@ -3,21 +3,13 @@ package model;
 import java.util.Date;
 
 public class seller_sns {
-<<<<<<< HEAD
+
 	private int ss_index;
 	private int s_index;
 	private String ss_sort;
 	private String ss_address;
 	private Date ss_date;
-=======
-	private int ss_index;		// index
-	private int s_index;		// 상점 index
-	private String ss_sort;		// sns 구분
-	private String ss_address;	// 주소
-	private Date ss_date;		// 등록일
-	
-	
->>>>>>> branch 'master' of https://github.com/StrokeLine/BitProject
+
 	public int getSs_index() {
 		return ss_index;
 	}
@@ -48,13 +40,11 @@ public class seller_sns {
 	public void setSs_date(Date ss_date) {
 		this.ss_date = ss_date;
 	}
-<<<<<<< HEAD
-=======
+
 	@Override
 	public String toString() {
 		return "seller_sns [ss_index=" + ss_index + ", s_index=" + s_index + ", ss_sort=" + ss_sort + ", ss_address="
 				+ ss_address + ", ss_date=" + ss_date + "]";
 	}
-	
->>>>>>> branch 'master' of https://github.com/StrokeLine/BitProject
+
 }
